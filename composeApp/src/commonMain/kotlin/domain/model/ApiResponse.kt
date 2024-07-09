e97ad21bccee2d4e57f2e538c28b727487cdd879
Guilherme Ignácio <stormierearth9@gmail.com>
2024-07-08 21:09:39 -0300

    Implement a local cache

--- composeApp/src/commonMain/kotlin/domain/model/ApiResponse.kt
+++ composeApp/src/commonMain/kotlin/domain/model/ApiResponse.kt
@@ -1,7 +1,10 @@
 package domain.model
 
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.mongodb.kbson.ObjectId
 
 @Serializable
 data class ApiResponse(
@@ -16,7 +19,11 @@ data class MetaData(
 )
 
 @Serializable
-data class Currency(
-    val code: String,
-    val value: Double
-)
+open class Currency : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var code: String = ""
+    var value: Double = 0.0
+
+    companion object // Attention: This is a workaround for a K2 compiler bug, this line may be needed to change in the future
+}
--- composeApp/src/commonMain/kotlin/presentation/screen/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/HomeViewModel.kt
@@ -2,17 +2,20 @@ package presentation.screen
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import domain.CurrencyApiService
+import domain.MongoRepository
 import domain.PreferencesRepository
 import domain.model.Currency
 import domain.model.RateStatus
 import domain.model.RequestState
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 
@@ -22,6 +25,7 @@ sealed class HomeUiEvent {
 
 class HomeViewModel(
     private val preferences: PreferencesRepository,
+    private val mongoDb: MongoRepository,
     private val api: CurrencyApiService
 ) : ScreenModel {
 
@@ -36,6 +40,9 @@ class HomeViewModel(
     private var _targetCurrency: MutableState<RequestState<Currency>> =
         mutableStateOf(RequestState.Idle)
     val targetCurrency: State<RequestState<Currency>> = _targetCurrency
+
+    private var _allCurrencies = mutableStateListOf<Currency>()
+    val allCurrencies: List<Currency> = _allCurrencies
 
     init {
         screenModelScope.launch {
@@ -56,9 +63,42 @@ class HomeViewModel(
 
     private suspend fun fetchNewRates() {
         try {
+            val localCache = mongoDb.readCurrencyData().first()
+            if (localCache.isSuccess()) {
+                if (localCache.getSuccessData().isNotEmpty()) {
+                    println("HomeViewModel: DATABASE IS FULL")
+                    _allCurrencies.addAll(localCache.getSuccessData())
+                    if (!preferences.isDataFresh(Clock.System.now().toEpochMilliseconds())) {
+                        println("HomeViewModel: DATA NOT FRESH")
+                        cacheData()
+                    } else {
+                        println("HomeViewModel: DATA IS FRESH")
+                    }
+                } else {
+                    println("HomeViewModel: DATABASE NEEDS DATA")
+                    cacheData()
+                }
+            } else if (localCache.isError()) {
+                println("HomeViewModel: ERROR READING LOCAL DATABASE ${localCache.getErrorData()}")
+            }
             api.getLatestExchangeRates()
         } catch (e: Exception) {
             println(e.stackTraceToString())
+        }
+    }
+
+    private suspend fun cacheData() {
+        val fetchData = api.getLatestExchangeRates()
+        if (fetchData.isSuccess()) {
+            mongoDb.cleanUp()
+            fetchData.getSuccessData().forEach {
+                println("HomeViewModel: ADDING ${it.code}")
+                mongoDb.insertCurrencyData(it)
+            }
+            println("HomeViewModel: UPDATING _allCurrencies")
+            _allCurrencies.addAll(fetchData.getSuccessData())
+        } else if (fetchData.isError()) {
+            println("HomeViewModel: Fething Failed ${fetchData.getErrorData()}")
         }
     }
 
